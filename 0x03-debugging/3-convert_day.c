d408c606a99f19395bf36d77845918deaebd9f09
hbapte <ijbapte@gmail.com>
2022-06-10 15:53:45 -0700

    task 2 & 4

--- 0x03-debugging/3-convert_day.c
+++ 0x03-debugging/3-convert_day.c
@@ -24,14 +24,31 @@ int convert_day(int month, int day)
 			day = 90 + day;
 			break;
 		case 5:
-			 day = 120 + day;
-			 break;
+			day = 120 + day;
+			break;
 		case 6:
 			day = 151 + day;
 			break;
 		case 7:
 			day = 181 + day;
 			break;
-	}
+		case 8:
+			day = 212 + day;
+			break;
+		case 9:
+			day = 243 + day;
+			break;
+		case 10:
+			day = 273 + day;
+			break;
+		case 11:
+			day = 304 + day;
+			break;
+		case 12:
+			day = 334 + day;
+			break;
+		default:
+			break;
+		}
 	return (day);
 }
